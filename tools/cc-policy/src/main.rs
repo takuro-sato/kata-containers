f5de52439266bff2fc11a6dd0a2165390a7731ac
Ming-Wei Shih <mishih@microsoft.com>
2023-02-02 22:33:08 +0000

    Change to REGO format
    Signed-off-by: Ming-Wei Shih <mishih@microsoft.com>

--- tools/cc-policy/src/main.rs
+++ tools/cc-policy/src/main.rs
@@ -57,24 +57,41 @@ fn get_policy_from_yaml(
 fn create_and_inject_policy(
     path: &PathBuf,
     with_default_rules: bool,
-) -> Result<(String, String, String)> {
+) -> Result<(Vec<(String, String)>, String)> {
     let yaml = read_to_string(path)?;
     let mut buffer = Vec::new();
     let mut ser = serde_yaml::Serializer::new(&mut buffer);
     let mut policy_list = Vec::new();
-    let mut policy_base64_list = Vec::new();
 
-    let yaml_from_dry_run = KubeCtl::get_yaml_with_dry_run_server(path)?;
+    let dry_run_result = KubeCtl::get_yaml_with_dry_run_server(path)?;
+
+    let mut v = Vec::new();
+    let mut dry_run_iter = if let Some(items) = dry_run_result.get("items") {
+        if let Some(seq) = items.as_sequence() {
+            seq.iter()
+        } else {
+            v.push(dry_run_result.clone());
+            v.iter()
+        }
+    } else {
+        v.push(dry_run_result.clone());
+        v.iter()
+    };
 
     for doc in serde_yaml::Deserializer::from_str(yaml.as_str()) {
         let mut yaml = serde_yaml::Value::deserialize(doc)?;
 
+        let yaml_dry_run = if let Some(dry_run_item) = dry_run_iter.next() {
+            dry_run_item
+        } else {
+            bail!("mismatched number of yaml from the dry run result");
+        };
+
         if let Ok((kind, policy, policy_base64)) =
-            get_policy_from_yaml(&yaml_from_dry_run, with_default_rules)
+            get_policy_from_yaml(yaml_dry_run, with_default_rules)
         {
             patch_yaml(&mut yaml, &kind, &policy_base64)?;
-            policy_list.push(policy.clone());
-            policy_base64_list.push(policy_base64.clone());
+            policy_list.push((policy.clone(), policy_base64.clone()));
         }
 
         yaml.serialize(&mut ser)?;
@@ -82,19 +99,16 @@ fn create_and_inject_policy(
 
     let yaml_with_policy = String::from_utf8_lossy(&buffer).to_string();
 
-    let policy = policy_list.join("\n");
-    let policy_base64 = policy_base64_list.join("\n");
-
-    Ok((policy, policy_base64, yaml_with_policy))
+    Ok((policy_list, yaml_with_policy))
 }
 
 fn create_policy_by_image_ref(
     image_ref: &str,
     with_default_rules: bool,
-) -> Result<(String, String)> {
+) -> Result<Vec<(String, String)>> {
     let policy = CcPolicy::from_image_ref(image_ref, with_default_rules)?;
 
-    Ok((policy.to_string(), policy.to_base64()))
+    Ok(vec![(policy.to_string(), policy.to_base64())])
 }
 
 fn write_to_file(data: &str, path: &PathBuf) -> Result<()> {
@@ -117,27 +131,41 @@ fn main() -> Result<()> {
         bail!("Cannot specify input_yaml and image_ref at the same time");
     }
 
-    let policy;
-    let policy_encoded;
+    let policy_list;
     let mut patched_yaml = String::new();
 
     if let Some(input_yaml) = args.input_yaml {
-        (policy, policy_encoded, patched_yaml) =
+        (policy_list, patched_yaml) =
             create_and_inject_policy(&input_yaml, args.with_default_rules)?;
     } else if let Some(image_ref) = args.image_ref {
-        (policy, policy_encoded) = create_policy_by_image_ref(&image_ref, args.with_default_rules)?;
+        policy_list = create_policy_by_image_ref(&image_ref, args.with_default_rules)?;
     } else {
         unreachable!();
     }
 
     if args.verbose {
-        println!("CcPolicy: {}", policy);
-        println!("Base64 encoding: {}", policy_encoded);
-        println!("Encoding size: {}", policy_encoded.len());
+        for (policy, policy_encoded) in &policy_list {
+            println!("{}", policy);
+            println!("Base64 encoding: {}", policy_encoded);
+            println!("Encoding size: {}", policy_encoded.len());
+        }
     }
 
     if let Some(output_policy) = args.output_policy {
-        write_to_file(&policy, &output_policy)?;
+        if policy_list.len() == 1 {
+            let (policy, _) = &policy_list[0];
+            write_to_file(policy, &output_policy)?;
+        } else {
+            for (index, (policy, _)) in policy_list.into_iter().enumerate() {
+                let path = output_policy.clone();
+                let output_name = path.file_stem().unwrap().to_string_lossy();
+                let output_ext = path.extension().unwrap().to_string_lossy();
+                let output_name = format!("{}{}.{}", output_name, index, output_ext);
+                let output_path = path.with_file_name(output_name);
+
+                write_to_file(&policy, &output_path)?;
+            }
+        }
     }
 
     if let Some(output_yaml) = args.output_yaml {
